7b2ef30a237ae8e9437ab633931610af6e7b70ad
thanhanz <daothanhanz95@gmail.com>
2026-05-31 18:46:33 +0700

    feat: add initial Docker setup and configuration for RiskTrace API

--- src/RiskTrace.Api/Program.cs
+++ src/RiskTrace.Api/Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RiskTrace.Infrastructure;
+using RiskTrace.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.MapGet("/", () => "Hello World!");
 
