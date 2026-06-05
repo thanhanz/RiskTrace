18bf6e8a8f6cb826e6fa44d2e575ed510302ae98
thanhanz <daothanhanz95@gmail.com>
2026-06-05 16:57:19 +0700

    feat(api): add Swagger documentation and update launch settings

--- src/RiskTrace.Api/Program.cs
+++ src/RiskTrace.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using RiskTrace.Infrastructure;
 using RiskTrace.Infrastructure.Persistence;
+using RiskTrace.UseCases;
 
 public partial class Program
 {
@@ -9,11 +11,27 @@ public partial class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "RiskTrace API",
+                Version = "v1"
+            });
+        });
+        builder.Services.AddUseCases();
 
         //Will remove soon - API should not depend on Infrastructure
         builder.Services.AddInfrastructure(builder.Configuration);
 
         var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
         using (var scope = app.Services.CreateScope())
         {
